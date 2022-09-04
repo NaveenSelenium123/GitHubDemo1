5cba6cb349b54da6fa925d833a6010e60612e67d
naveen <naveen@gmail.com>
2022-09-04 20:15:35 +0530

    Smple selenium2 code

---
+++ src/org/wipro/Sample123.java
@@ -0,0 +1,12 @@
+package org.wipro;
+
+public class Sample123 {
+	public Sample123() {
+		// TODO Auto-generated constructor stub
+	
+private  sysout1() {
+System.out.println("Git Bash Project123");
+
+}
+	}
+}
